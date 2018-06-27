a9d587fc277a3968e2f0b8730f2a57e9c8bea3c8
Davin Ahn <m.davinahn@gmail.com>
2018-06-28 01:57:06 +0900

    오프라인 환경에서도 문제 셋이 있으면 읽어올 수 있도록

--- src/main/API.js
+++ src/main/API.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import fs from 'fs';
+import isOnline from 'is-online';
 import path from 'path';
 import { dataPath } from 'main/Path';
 
@@ -7,43 +8,49 @@ const timeout = 1000 * 10;
 const base = 'https://api.github.com/repos/DavinAhn/learning-japanese';
 
 const fetchData = (dataSHA, updated, error) => {
-  axios.get(`${base}/contents/data?ref=master`, { timeout })
-    .then((response) => {
-      const fileInfo = response.data.find(item => item.name === path.basename(dataPath));
-      if (!fileInfo) {
-        error('Invaild fileinfo.');
-        return;
-      }
-
-      const sha = fileInfo.sha;
-      if (dataSHA && dataSHA === sha) {
-        updated(false, sha);
-        return;
-      }
-
-      const downloadURL = fileInfo.download_url;
-      if (!downloadURL) {
-        error('Invaild download url.');
-        return;
-      }
-
-      axios.get(downloadURL, { timeout, responseType: 'stream' })
+  isOnline({ timeout: 1000 }).then(online => {
+    if (online) {
+      axios.get(`${base}/contents/data?ref=master`, { timeout })
         .then((response) => {
-          const stream = response.data.pipe(fs.createWriteStream(dataPath));
-          stream.on('finish', () => {
-            updated(true, sha);
-          });
-          stream.on('error', (e) => {
-            error(e);
-          });
+          const fileInfo = response.data.find(item => item.name === path.basename(dataPath));
+          if (!fileInfo) {
+            error('Invaild fileinfo.');
+            return;
+          }
+    
+          const sha = fileInfo.sha;
+          if (dataSHA && dataSHA === sha) {
+            updated(false, sha);
+            return;
+          }
+    
+          const downloadURL = fileInfo.download_url;
+          if (!downloadURL) {
+            error('Invaild download url.');
+            return;
+          }
+    
+          axios.get(downloadURL, { timeout, responseType: 'stream' })
+            .then((response) => {
+              const stream = response.data.pipe(fs.createWriteStream(dataPath));
+              stream.on('finish', () => {
+                updated(true, sha);
+              });
+              stream.on('error', (e) => {
+                error(e);
+              });
+            })
+            .catch((e) => {
+              error(e);
+            });
         })
         .catch((e) => {
           error(e);
         });
-    })
-    .catch((e) => {
-      error(e);
-    });
+    } else {
+      updated(false, dataSHA);
+    }
+  });
 };
 
 export default { fetchData }
