4310fd1c92d25ecc83d8e2dc45d42abb3de3c6f7
Davin Ahn <m.davinahn@gmail.com>
2018-07-08 01:00:11 +0900

    데이터 업데이트 체크는 10분에 한번만 하도록

--- src/main/API.js
+++ src/main/API.js
@@ -4,12 +4,16 @@ import isOnline from 'is-online';
 import path from 'path';
 import { dataPath } from 'main/Path';
 
+const fetchInterval = 1000 * 60 * 10;
 const timeout = 1000 * 10;
 const base = 'https://api.github.com/repos/DavinAhn/learning-japanese';
 
+let limit = 0;
+
 const fetchData = (dataSHA, updated, error) => {
+  let time = new Date().getTime();
   isOnline({ timeout: 1000 }).then(online => {
-    if (online) {
+    if (!online && limit < time) {
       axios.get(`${base}/contents/data?ref=master`, { timeout })
         .then((response) => {
           const fileInfo = response.data.find(item => item.name === path.basename(dataPath));
@@ -20,6 +24,7 @@ const fetchData = (dataSHA, updated, error) => {
     
           const sha = fileInfo.sha;
           if (dataSHA && dataSHA === sha) {
+            limit = time + fetchInterval;
             updated(false, sha);
             return;
           }
@@ -34,6 +39,7 @@ const fetchData = (dataSHA, updated, error) => {
             .then((response) => {
               const stream = response.data.pipe(fs.createWriteStream(dataPath));
               stream.on('finish', () => {
+                limit = time + fetchInterval;
                 updated(true, sha);
               });
               stream.on('error', (e) => {
