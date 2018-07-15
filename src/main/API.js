8c7e0c82b1408d1c63fef15a84bdafd03478b36e
Davin Ahn <m.davinahn@gmail.com>
2018-07-15 19:20:57 +0900

    데이터 업데이트 체크 주기가 라이프사이클에 의존하지 않도록

--- src/main/API.js
+++ src/main/API.js
@@ -2,18 +2,25 @@ import axios from 'axios';
 import fs from 'fs';
 import isOnline from 'is-online';
 import path from 'path';
+import settings from 'electron-settings';
 import { dataPath } from 'main/Path';
 
-const fetchInterval = 1000 * 60 * 10;
 const timeout = 1000 * 10;
 const base = 'https://api.github.com/repos/DavinAhn/learning-japanese';
 
-let limit = 0;
+const getFetchLimit = () => {
+  return settings.get('app.fetchLimit') || 0;
+};
+
+const updateFetchLimit = () => {
+  const time = new Date().getTime();
+  settings.set('app.fetchLimit', time + 1000 * 60 * 10);
+};
 
 const fetchData = (dataSHA, updated, error) => {
-  let time = new Date().getTime();
   isOnline({ timeout: 1000 }).then(online => {
-    if (online && limit < time) {
+    const time = new Date().getTime();
+    if (online && getFetchLimit() < time) {
       axios.get(`${base}/contents/data?ref=master`, { timeout })
         .then((response) => {
           const fileInfo = response.data.find(item => item.name === path.basename(dataPath));
@@ -24,7 +31,7 @@ const fetchData = (dataSHA, updated, error) => {
     
           const sha = fileInfo.sha;
           if (dataSHA && dataSHA === sha) {
-            limit = time + fetchInterval;
+            updateFetchLimit();
             updated(false, sha);
             return;
           }
@@ -39,7 +46,7 @@ const fetchData = (dataSHA, updated, error) => {
             .then((response) => {
               const stream = response.data.pipe(fs.createWriteStream(dataPath));
               stream.on('finish', () => {
-                limit = time + fetchInterval;
+                updateFetchLimit();
                 updated(true, sha);
               });
               stream.on('error', (e) => {
